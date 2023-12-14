56f2b20458d040d45c33f6187c565fd96c514a29
Okekunle  Oluwatobiloba Samuel <47566668+Oluwatobilobaoke@users.noreply.github.com>
2023-12-14 14:32:53 +0100

    Update ProductRate.js

--- src/components/ProductRate.js
+++ src/components/ProductRate.js
@@ -1,1 +1,15 @@
 
+"use client";
+
+import "@smastrom/react-rating/style.css";
+import { Rating } from "@smastrom/react-rating";
+
+export default function ProductRate({ rate, count }) {
+  return (
+    <div className="flex">
+      <Rating style={{ width: 100 }} value={rate} readonly />
+
+      <span className="ml-2">{count} reviews</span>
+    </div>
+  );
+}
